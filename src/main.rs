b2fe42a6ea7d02175fd1c49d299c122b04b65add
David Tolnay <dtolnay@gmail.com>
2022-10-17 13:39:42 -0700

    Add link from rustdoc to github and crates.io

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+//! [![github]](https://github.com/dtolnay/dircnt)&ensp;[![crates-io]](https://crates.io/crates/dircnt)&ensp;[![docs-rs]](https://docs.rs/dircnt)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+
 #![allow(clippy::let_underscore_drop, clippy::uninlined_format_args)]
 
 use std::env;
