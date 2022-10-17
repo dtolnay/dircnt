5f9f08c39484c9660ff968562ba22960ce0aa265
David Tolnay <dtolnay@gmail.com>
2022-10-17 13:21:18 -0700

    dircnt crate

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs;
+use std::io::{self, Write};
+use std::process;
+
+fn main() {
+    if let Err(error) = try_main() {
+        let _ = writeln!(io::stderr(), "dircnt: {}", error);
+        process::exit(1);
+    }
+}
+
+fn try_main() -> io::Result<()> {
+    let mut args_os = env::args_os().skip(1);
+    let dir = args_os.next();
+
+    let read_dir = match dir {
+        Some(dir) => fs::read_dir(dir),
+        None => fs::read_dir("."),
+    }?;
+
+    let mut count = 0usize;
+    for entry in read_dir {
+        entry?;
+        count += 1;
+    }
+    let _ = writeln!(io::stdout(), "{}", count);
+    Ok(())
+}
